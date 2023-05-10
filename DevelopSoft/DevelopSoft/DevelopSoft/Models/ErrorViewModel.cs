80d56b013b0703814cdd5dba430a1adcb832daa0
IvanovvAlex <alexivanovv4@gmail.com>
2023-05-10 18:50:57 +0300

    Init commit + Add DbContext + Add Entity Models

---
+++ DevelopSoft/DevelopSoft/DevelopSoft/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevelopSoft.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
